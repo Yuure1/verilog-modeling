ceb09e9a2ce481e9fb122c99160a36b35f6cb2ae
MrRobot <weebforever12@gmail.com>
2026-01-15 22:15:02 +0800

    first commit

---
+++ gate_level_modeling/xor_gate/testbench.v
@@ -0,0 +1,13 @@
+module testbench;
+    reg a, b;
+    wire y;
+
+    xor_gate dut(.Y(y), .A(a), .B(b));
+
+    initial begin
+        a=0; b=0; #10;
+        a=0; b=1; #10;
+        a=1; b=0; #10;
+        a=1; b=1; #10;
+    end
+endmodule
